9acc6426b627a17d34b96afef170c37cdac8ddab
cgarret184 <cgarret184@gmail.com>
2026-01-22 21:47:38 +0000

    vibe coded the hell outta this

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ const int PALETTE_SIZE = 5;
 
 int red[PALETTE_SIZE]   = { 54, 250,  90,   10, 5 };
 int green[PALETTE_SIZE] = { 243,  70,  10,   100, 25 };
-int blue[PALETTE_SIZE]  = { 100, 260, 92, 150, 205 };
+int blue[PALETTE_SIZE]  = { 100, 100, 92, 150, 205 };
 
 int main() {
     const int WIDTH = 800;
@@ -28,11 +28,19 @@ int main() {
     out << "const canvas = document.getElementById('c');\n";
     out << "const ctx = canvas.getContext('2d');\n";
     out << "const img = ctx.createImageData(" << WIDTH << ", " << HEIGHT << ");\n";
+    out << "let data = img.data;\n\n";
 
-    // Push Mandelbrot data into JS arrays
-    out << "let data = img.data;\n";
+    // Define palette first
+    out << "let palette = [\n";
+    for (int i = 0; i < PALETTE_SIZE; i++) {
+        out << "  {r: " << red[i] << ", g: " << green[i] << ", b: " << blue[i] << "}";
+        if (i < PALETTE_SIZE - 1) out << ",";
+        out << "\n";
+    }
+    out << "];\n\n";
 
-    out << "let i = 0;\n";
+    // Single loop: calculate Mandelbrot and color pixels
+    out << "let idx = 0;\n";
     out << "for (let y = 0; y < " << HEIGHT << "; y++) {\n";
     out << "  for (let x = 0; x < " << WIDTH << "; x++) {\n";
     out << "    let zx = 0, zy = 0;\n";
@@ -45,33 +53,16 @@ int main() {
     out << "      zx = tmp;\n";
     out << "      iter--;\n";
     out << "    }\n";
-
-    // Color the pixels based on iteration count
-    // iter is the number of iterations used for this pixel
-    // Choose an index into the palette based on iter
     out << "    let index = iter % " << PALETTE_SIZE << ";\n";
-    
-    // Output pixel color data for all palette indices into JavaScript
-    // This creates an array of color objects that will be used to look up colors
-    out << "    let palette = [\n";
-    for (int i = 0; i < PALETTE_SIZE; i++) {
-        out << "      {r: " << red[i] << ", g: " << green[i] << ", b: " << blue[i] << "}";
-        if (i < PALETTE_SIZE - 1) out << ",";
-        out << "\n";
-    }
-    out << "    ];\n";
-    
-    // Get the color from the palette for this pixel's iteration count
     out << "    let color = palette[index];\n";
-    // Write the red, green, and blue values to the image data
-    out << "    data[i++] = color.r;\n";
-    out << "    data[i++] = color.g;\n";
-    out << "    data[i++] = color.b;\n";
-    out << "    data[i++] = 255;\n"; // Alpha channel is always fully opaque (255)
-    // Close the x and y loops
-    out << "    }\n";
+    out << "    data[idx++] = color.r;\n";
+    out << "    data[idx++] = color.g;\n";
+    out << "    data[idx++] = color.b;\n";
+    out << "    data[idx++] = 255;\n";
     out << "  }\n";
-    out << "}\n";
+    out << "}\n\n";
+    
+
     
     out << "ctx.putImageData(img, 0, 0);\n";
     out << "</script>\n</body>\n</html>\n";
