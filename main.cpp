6ef091dbb3828987b94fe0487c7f7ae018122a4e
cgarret184 <cgarret184@gmail.com>
2026-01-22 21:36:51 +0000

    added code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 #include <iostream>
 #include <fstream>
 
+// You can change these colors and add more.
+const int PALETTE_SIZE = 5;
+
+int red[PALETTE_SIZE]   = { 54, 250,  90,   10, 5 };
+int green[PALETTE_SIZE] = { 243,  70,  10,   100, 25 };
+int blue[PALETTE_SIZE]  = { 100, 260, 92, 150, 205 };
+
 int main() {
     const int WIDTH = 800;
     const int HEIGHT = 600;
@@ -39,15 +46,33 @@ int main() {
     out << "      iter--;\n";
     out << "    }\n";
 
-    // Color 
-    out << "    let color = iter | (iter << 8);\n";
-    out << "    data[i++] = (color >> 16) & 255;\n";
-    out << "    data[i++] = (color >> 8) & 255;\n";
-    out << "    data[i++] = color & 255;\n";
-    out << "    data[i++] = 255;\n";   // alpha
+    // Color the pixels based on iteration count
+    // iter is the number of iterations used for this pixel
+    // Choose an index into the palette based on iter
+    out << "    let index = iter % " << PALETTE_SIZE << ";\n";
+    
+    // Output pixel color data for all palette indices into JavaScript
+    // This creates an array of color objects that will be used to look up colors
+    out << "    let palette = [\n";
+    for (int i = 0; i < PALETTE_SIZE; i++) {
+        out << "      {r: " << red[i] << ", g: " << green[i] << ", b: " << blue[i] << "}";
+        if (i < PALETTE_SIZE - 1) out << ",";
+        out << "\n";
+    }
+    out << "    ];\n";
+    
+    // Get the color from the palette for this pixel's iteration count
+    out << "    let color = palette[index];\n";
+    // Write the red, green, and blue values to the image data
+    out << "    data[i++] = color.r;\n";
+    out << "    data[i++] = color.g;\n";
+    out << "    data[i++] = color.b;\n";
+    out << "    data[i++] = 255;\n"; // Alpha channel is always fully opaque (255)
+    // Close the x and y loops
+    out << "    }\n";
     out << "  }\n";
     out << "}\n";
-
+    
     out << "ctx.putImageData(img, 0, 0);\n";
     out << "</script>\n</body>\n</html>\n";
 
